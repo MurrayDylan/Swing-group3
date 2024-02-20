83f308cdf574d177f9a6083e35e2ddc369801b89
Anand <sainbila@tcd.ie>
2024-02-20 13:19:51 +0000

    Better functionality, added home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
-import LoginButton from "./components/login";
-import LogoutButton from "./components/logout";
-import Profile from "./components/profile";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/login';
+import Home from './components/Home';
+import Profile from './components/profile';
+import LogoutButton from './components/logout';
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-    <LoginButton></LoginButton>
-    <Profile></Profile>
-    <LogoutButton></LogoutButton>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" replace />} />
+        <Route path="/login" element={!isAuthenticated ? <LoginButton /> : <Navigate to="/" replace />} />
+        <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/login" replace />} />
+        <Route path="/logout" element={isAuthenticated ? <LogoutButton /> : <Navigate to="/login" replace />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
