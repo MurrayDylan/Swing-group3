c829938aad430f5984a691610fe07d97ed546296
Al <114831941+kataokaa@users.noreply.github.com>
2024-02-23 19:33:04 +0000

    Delete backend/idk directory
    meant to add to my own branch

--- backend/idk/src/main/java/idk/idk/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package idk.idk;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-    User findUserById(Integer id);
-}
