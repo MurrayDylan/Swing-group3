65a63bd33fac38fdf540bcaaf474c25e48aa7425
Al <kataokaa@tcd.ie>
2024-02-25 20:31:58 +0000

    fixed last commit

---
+++ backend/dashboard/src/main/java/com/groupwork/dashboard/UserController.java
@@ -0,0 +1,30 @@
+package com.groupwork.dashboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/add")
+    public String addUser(@RequestParam String first, @RequestParam String last) {
+        User user = new User();
+        user.setFirstName(first);
+        user.setLastName(last);
+        userRepository.save(user);
+        return "Added new customer to repo!";
+    }
+
+    @GetMapping("/list")
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public User findUserById(@PathVariable Integer id) {
+        return userRepository.findUserById(id);
+    }
+}
