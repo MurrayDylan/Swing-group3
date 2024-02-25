63367f654bfed7c6a2eca90254e527aeac1c5558
Al <kataokaa@tcd.ie>
2024-02-25 20:32:58 +0000

    fixed last commit

---
+++ backend/dashboard/src/main/java/com/groupwork/dashboard/DashboardApplication.java
@@ -0,0 +1,13 @@
+package idk.idk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+
+}
