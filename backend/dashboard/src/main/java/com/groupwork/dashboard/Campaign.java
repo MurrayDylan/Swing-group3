0ed586def00bca191d62f8af28e25e4686f0ba95
Al <kataokaa@tcd.ie>
2024-02-27 00:43:16 +0000

    added mysql connector

--- backend/dashboard/src/main/java/com/groupwork/dashboard/Campaign.java
+++ backend/dashboard/src/main/java/com/groupwork/dashboard/Campaign.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
 public class Campaign{
     private String name;
 private int budget;
@@ -14,6 +13,15 @@ private int kpi;
 private int baseBid;
 private int maxBid;
 private int CPM;
+    public Campaign(String name, int budget, int spend, int kpi, int baseBid, int maxBid, int CPM) {
+        this.name = name;
+        this.budget = budget;
+        this.spend = spend;
+        this.kpi = kpi;
+        this.baseBid = baseBid;
+        this.maxBid = maxBid;
+        this.CPM = CPM;
+    }
 private String frequency;
     public int getBudget(){
         return this.budget;
@@ -28,5 +36,34 @@ private String frequency;
         this.budget= spendSet;
     }
 
+    public int getKpi() {
+        return kpi;
+    }
+
+    public void setKpi(int kpi) {
+        this.kpi = kpi;
+    }
+    public int getBaseBid() {
+        return baseBid;
+    }
+
+    public void setBaseBid(int baseBid) {
+        this.baseBid = baseBid;
+    }
+
+    public int getMaxBid() {
+        return maxBid;
+    }
+
+    public void setMaxBid(int maxBid) {
+        this.maxBid = maxBid;
+    }
+
+    public int getCPM() {
+        return CPM;
+    }
+    public void setCPM(int CPM) {
+        this.CPM = CPM;
+    }
 }
 
--- backend/dashboard/src/main/java/com/groupwork/dashboard/UserController.java
+++ backend/dashboard/src/main/java/com/groupwork/dashboard/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping(path="/user")
+@RequestMapping(path="/user")
 public class UserController {
 
 
