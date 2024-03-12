34595a3bfbbcf7d9c2c19dc29a0ee1f1a0d1b11d
MULASAKEE <suny3@tcd.ie>
2024-03-12 17:28:50 +0000

    new list for profile of Career and Gender

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -28,13 +28,16 @@ const ProfileEdit = () => {
         onChange={handleChange}
         placeholder="Name"
       />
-      <input
-        type="text"
-        name="gender"
-        value={profile.gender}
-        onChange={handleChange}
-        placeholder="Gender"
-      />
+      {/* Gender dropdown */}
+      <select name="gender" value={profile.gender} onChange={handleChange}>
+      <option value="">Select Gender</option>
+      {/* Add gender options here */}
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+      <option value="nonbinary">Non-Binary</option>
+      <option value="tescoshoppingbag">Tesco Shopping Bag</option>
+      <option value="prefernottosay">Prefer Not To Say</option>
+      </select>
       <input
         type="date"
         name="dateOfBirth"
@@ -55,13 +58,30 @@ const ProfileEdit = () => {
         onChange={handleChange}
         placeholder="Payment Method"
       />     
-      <input
-        type="text"
-        name="career"
-        value={profile.career}
-        onChange={handleChange}
-        placeholder="Career"
-      />   
+      {/* Career dropdown */}
+      <select name="career" value={profile.career} onChange={handleChange}>
+      <option value="">Select Career</option>
+      {/* Add career options here */}
+      <option value="generaloccupations">General Occupations</option>
+      <option value="education">Education</option>
+      <option value="agriculturefisheriestimberandforestry">Agriculture, Fisheries, Timber and Forestry</option>
+      <option value="construction">Construction</option>
+      <option value="miningandextractiveindustries">Mining and extractive industries</option>
+      <option value="transportation">Transportation</option>
+      <option value="manufacturingprocessingandrepair">Manufacturing, processing and repair</option>
+      <option value="publishingandadvertising">Publishing and advertising</option>
+      <option value="medicalandhealthcare">Medical and Health Care</option>
+      <option value="religiousorganization">Religious Organization</option>
+      <option value="publicservice">Public Service</option>
+      <option value="business">Business</option>
+      <option value="professionalsports">Professional Sports</option>
+      <option value="students">Students</option>
+      <option value="unemployed">Unemployed</option>
+      <option value="selfemployed">Self-Employed</option>
+      <option value="prefernottosay">Prefer Not To Say</option>
+      
+      {/* etc. */}
+  </select>
       <button onClick={handleSave}>Save</button>
     </div>
   );
