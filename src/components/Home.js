d4d0d1f5f0f6aef94a4d50fd4803a8b733ae5081
Anand <sainbila@tcd.ie>
2024-03-13 01:12:30 +0000

    small fixes for the logo and profile image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './Home.css'; // Import the CSS file
-import logoImage from '../assets/finch-logo.png'; // Make sure the path to your logo is correct
-import videoBackground from '../assets/LiveBackground2.mp4'; // Import the video file
+import logoImage from '../assets/finch-logo.png'; 
+//import videoBackground from '../assets/LiveBackground2.mp4'; // Import the video file
 
 function Home() {
   return (
-    <div className="white-text"> {/* Apply the white-text class */}
-      <div className="video-background">
-        <video autoplay="autoplay" loop="loop" muted="muted" playsInline>
-          <source src={videoBackground} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+    <>
+      <img src={logoImage} alt="Finch Mini Trade Desk Logo" className="App-logo" />
+      <div className="white-text"> {/* Apply the white-text class */}
+        {/* <div className="video-background"> */}
+          {/* <video autoplay="autoplay" loop="loop" muted="muted" playsInline>
+            <source src={videoBackground} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video> */}
+        {/* </div> */}
+        <div className='home'>
+          <div className="welcome-container">
+            <h1>Welcome to Finch Mini Trade Desk</h1>
+          </div>
+          <div className="description-container">
+            <p className="description">
+              Here is a brief description about Finch Mini Trade Desk. Learn more about our features, services, and how we can help you manage your trades more efficiently.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className='home'>
-        <div className="logo-container">
-          <img src={logoImage} alt="Finch Mini Trade Desk Logo" className="App-logo" />
-        </div>
-        <div className="welcome-container">
-          <h1>Welcome to Finch Mini Trade Desk</h1>
-        </div>
-        <div className="description-container">
-          <p className="description">
-            Here is a brief description about Finch Mini Trade Desk. Learn more about our features, services, and how we can help you manage your trades more efficiently.
-          </p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
